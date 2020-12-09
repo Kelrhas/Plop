0143134e2cb2f51ab0c58417ec5ef57eed3599db
Kelrhas <flbonnin@gmail.com>
2020-12-09 13:51:28 +0100

    Fix open visual studio from allocation tracking

--- Plop/src/Debug/Debug.cpp
+++ Plop/src/Debug/Debug.cpp
@@ -8,6 +8,75 @@
 
 #include <Debug/MemoryTrack.hpp>
 
+
+
+#include <atlbase.h>
+//-----------------------------------------------------------------------
+// This code is blatently stolen from http://benbuck.com/archives/13
+//
+// This is from the blog of somebody called "BenBuck" for which there
+// seems to be no information.
+//-----------------------------------------------------------------------
+
+// import EnvDTE
+#pragma warning(disable : 4278)
+#pragma warning(disable : 4146)
+#import "libid:80cc9f66-e7d8-4ddd-85b6-d9e6cd0e93e2" version("8.0") lcid("0") raw_interfaces_only named_guids
+#pragma warning(default : 4146)
+#pragma warning(default : 4278)
+
+bool visual_studio_open_file( char const* filename, unsigned int line )
+{
+	HRESULT result;
+	CLSID clsid;
+	result = ::CLSIDFromProgID( L"VisualStudio.DTE", &clsid );
+	if (FAILED( result ))
+		return false;
+
+	CComPtr<IUnknown> punk;
+	result = ::GetActiveObject( clsid, NULL, &punk );
+	if (FAILED( result ))
+		return false;
+
+	CComPtr<EnvDTE::_DTE> DTE;
+	DTE = punk;
+
+	CComPtr<EnvDTE::ItemOperations> item_ops;
+	result = DTE->get_ItemOperations( &item_ops );
+	if (FAILED( result ))
+		return false;
+
+	CComBSTR bstrFileName( filename );
+	CComBSTR bstrKind( EnvDTE::vsViewKindTextView );
+	CComPtr<EnvDTE::Window> window;
+	result = item_ops->OpenFile( bstrFileName, bstrKind, &window );
+	if (FAILED( result ))
+		return false;
+
+	CComPtr<EnvDTE::Document> doc;
+	result = DTE->get_ActiveDocument( &doc );
+	if (FAILED( result ))
+		return false;
+
+	CComPtr<IDispatch> selection_dispatch;
+	result = doc->get_Selection( &selection_dispatch );
+	if (FAILED( result ))
+		return false;
+
+	CComPtr<EnvDTE::TextSelection> selection;
+	result = selection_dispatch->QueryInterface( &selection );
+	if (FAILED( result ))
+		return false;
+
+	result = selection->GotoLine( line, TRUE );
+	if (FAILED( result ))
+		return false;
+
+	return true;
+}
+
+
+
 namespace Plop
 {
 	namespace Debug
@@ -63,7 +132,7 @@ namespace Plop
 			{
 #ifdef ENABLE_MEMORY_TRACKING
 				ASSERT( s_GlobalAllocation.uCurrentNumberAllocation == s_GlobalAllocation.mapAllocations.size(), "size mismatch" );
-				if (ImGui::Button( "Clear current allocations" ))
+				if (ImGui::Button( "Clear current tracking" ))
 				{
 					s_GlobalAllocation.mapAllocations.clear();
 					s_GlobalAllocation.mapAllocationsCalls.clear();
@@ -86,18 +155,22 @@ namespace Plop
 					size_t uSize = kv.first;
 					AllocationLocation location = kv.second;
 
-					static char buff[256]{ 0 };
-					sprintf_s( buff, "%s:%llu", location.pFile, location.uLine );
-					if (ImGui::Selectable( buff ))
+					std::ostringstream ssFileLine;
+					ssFileLine << location.pFile << ":" << location.uLine;
+					if (ImGui::Selectable( ssFileLine.str().c_str() ))
 					{
 #ifdef PLATFORM_WINDOWS
-						sprintf_s( buff, "/edit %s /command \"edit.goto %llu\"", location.pFile, location.uLine );
-						HINSTANCE res = ShellExecuteA( NULL, "runas", "devenv", buff, NULL, SW_SHOW );
-						ASSERT( (size_t)res > 32, "Error while executing command: <{0}> returned {1}", buff, (size_t)res );
+						visual_studio_open_file( location.pFile, (unsigned int)location.uLine );
+						//sprintf_s( buff, "/edit %s /command \"edit.goto %llu\"", location.pFile, location.uLine );
+						//HINSTANCE res = ShellExecuteA( NULL, "runas", "devenv", buff, NULL, SW_SHOW );
+						//ASSERT( (size_t)res > 32, "Error while executing command: <{0}> returned {1}", buff, (size_t)res );
 #endif
 					}
-					ImGui::SameLine( ImGui::GetWindowContentRegionMax().x - 100 );
-					ImGui::Text( "%llu bytes", uSize );
+					std::ostringstream ssBytes;
+					ssBytes << uSize << " bytes";
+					ImVec2 vTextSize = ImGui::CalcTextSize( ssBytes.str().c_str() );
+					ImGui::SameLine( ImGui::GetWindowContentRegionMax().x - vTextSize.x );
+					ImGui::Text( ssBytes.str().c_str() );
 				}
 				ImGui::EndChild();
 

ef27f1c5033341359e4e03ec35576e5e073b3b02
Kelrhas <flbonnin@gmail.com>
2021-03-08 09:49:54 +0100

    Forgot file for allocation tracking on OS level

--- Plop/src/Utils/StringUtils.h
+++ Plop/src/Utils/StringUtils.h
@@ -3,6 +3,7 @@
 #include <algorithm> 
 #include <cctype>
 #include <locale>
+#include <sstream>
 
 // https://stackoverflow.com/a/217605
 
@@ -31,4 +32,22 @@ namespace Utils
 		TrimRight( s );
 	}
 
+
+	static inline std::string SizeToString( size_t _size )
+	{
+		static const char* suffixes[] = { "B", "KB", "MB", "GB", "TB", "PB", "EB" };
+
+		int suffixIndex = 0;
+		while ((_size >> (10*suffixIndex)) > 1024)
+		{
+			++suffixIndex;
+		}
+
+		double dFinalSize = _size / (double)( 1 << (10llu * suffixIndex));
+
+		std::ostringstream ss;
+		ss.precision( 3 );
+		ss << std::fixed << dFinalSize << " " << suffixes[suffixIndex];
+		return ss.str();
+	}
 }

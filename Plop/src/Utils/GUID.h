880cfbd2cccfc1783e71559272701f1b6b4ed81d
Kelrhas <flbonnin@gmail.com>
2023-01-18 21:55:21 +0100

    [Plop]Tweak GUID

--- Plop/src/Utils/GUID.h
+++ Plop/src/Utils/GUID.h
@@ -5,10 +5,10 @@ namespace Plop
 	struct GUID
 	{
 		using base_t = uint64_t;
-		static_assert(std::is_trivially_copyable_v<GUID::base_t>);
+		static_assert(std::is_integral_v<base_t>);
 
 		GUID();
-		constexpr GUID(const base_t _uID) : m_uID(_uID) {}
+		constexpr GUID(const base_t _uID) noexcept : m_uID(_uID) {}
 		explicit operator base_t() const { return m_uID; }
 		GUID &	 operator=(const GUID &_o) = default;
 		bool	 operator==(const GUID &_o) const { return m_uID == _o.m_uID; }
@@ -17,7 +17,7 @@ namespace Plop
 		static const Plop::GUID INVALID;
 
 	private:
-		base_t m_uID;
+		base_t m_uID = 0;
 	};
 	static_assert(std::is_trivially_copyable_v<GUID>);
 } // namespace Plop
